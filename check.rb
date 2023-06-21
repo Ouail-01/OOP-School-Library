14de0d9b86d83e838a02e47275f3e60c084fff0e
Ouail-01 <ouaillaarabi@gmail.com>
2023-06-21 05:29:33 +0100

    Fix the linters

--- check.rb
+++ check.rb
@@ -1,10 +1,10 @@
-require_relative 'person';
-require_relative 'capitalize_decorator';
-require_relative 'trimmer_decorator';
+require_relative 'person'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
 p person.correct_name
-p capitalizedPerson = CapitalizeDecorator.new(person)
-p capitalizedPerson.correct_name
-p capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-p capitalizedTrimmedPerson.correct_name
+p capitalized_person = CapitalizeDecorator.new(person)
+p capitalized_person.correct_name
+p capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
